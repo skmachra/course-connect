868ab3e818ea6a6eebd67f952ff4f349603e3999
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2024-10-20 08:20:02 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
